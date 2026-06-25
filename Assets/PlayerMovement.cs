6a5e9f2cd4ff5911fa1d193debe7bfd024e26440
geras97 <ivan.gtc13@gmail.com>
2026-06-25 17:50:12 -0600

    Player Movement
    Se agrego el movimiento del jugador

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float mouseSensitivity = 1000f;
+    public float gravity = -9.81f;
+
+    float xRotation = 0f;
+    float yVelocity;
+
+    public float jumpHeight = 2;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.forward * z + transform.right * x;
+
+        if (direction.magnitude > 1f)
+            direction.Normalize();
+
+        Vector3 move = direction * speed + Vector3.up * yVelocity;
+
+        controller.Move(move * Time.deltaTime);
+    }
+}
