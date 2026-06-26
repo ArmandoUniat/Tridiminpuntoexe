73a7cb802837af0739a9b70f10234f3384066493
SpeedWagonJC <armando.cerda@uniat.edu.mx>
2026-06-26 02:00:52 -0600

    .

--- Assets/Scrips/scrip.cs
+++ Assets/Scrips/scrip.cs
@@ -1,14 +1,28 @@
+//using UnityEngine;
+
+//public class ToggleUI : MonoBehaviour
+//{
+//   [SerializeField] private Canvas uiCanvas;
+//
+//   void Update()
+//    {
+//        if (Input.GetMouseButtonDown(1))
+//        {
+//            uiCanvas.enabled = !uiCanvas.enabled;
+//        }
+//    }
+//}
 using UnityEngine;
 
 public class ToggleUI : MonoBehaviour
 {
-    [SerializeField] private Canvas uiCanvas;
+    [SerializeField] private GameObject canvas;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            uiCanvas.enabled = !uiCanvas.enabled;
+            canvas.SetActive(!canvas.activeSelf);
         }
     }
 }
