7ce286fe7279e360cc24c2b82b580ea0512c9543
SpeedWagonJC <armando.cerda@uniat.edu.mx>
2026-06-25 23:26:26 -0600

    timer

--- Assets/Scrips/Timer.cs
+++ Assets/Scrips/Timer.cs
@@ -1,6 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Timer
+public class Timer : MonoBehaviour
 {
-    
+
+    public float timer = 0;
+
+    public Text textoTimer;
+
+    void Update()
+    {
+        timer = timer - 1;
+
+        textoTimer.text = "" + timer.ToString();
+
+
+    }
 }
